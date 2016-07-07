b4d5eb295be781d6c4c67bdd5de38d1aac24c542
Gonzalo Villarino <ghv8@cornell.edu>
2016-07-07 19:34:08 -0400

    Update samToIGV.sh

--- samToIGV.sh
+++ samToIGV.sh
@@ -2,3 +2,10 @@
 samtools view -Sb $1.sam > $1.bam
 samtools sort $1.bam $1.sorted
 samtools index $1.sorted.bam
+
+# I think is this one
+
+#!/bin/sh
+samtools view -bS $1 > $1.bam
+samtools sort $1.bam $1.sorted
+samtools index $1.sorted.bam
