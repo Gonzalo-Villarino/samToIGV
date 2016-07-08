952e21bdc38c3283ce84aa36c3c2350ec3b1c385
Gonzalo Villarino <ghv8@cornell.edu>
2016-07-07 20:43:15 -0400

    Update index.sh

--- index.sh
+++ index.sh
@@ -1,4 +1,26 @@
-#!/bin/sh
-./samToIGV.sh AP1_day2_rep2 & 
-./samToIGV.sh SEP3_day2_rep1 &
-./samToIGV.sh SEP3_day2_rep2
+#!/bin/bash
+./samToIGV.sh BW_16C_atxr.R1 &
+./samToIGV.sh BW_16C_atxr.R2 &
+./samToIGV.sh BW_16C_atxr.R3 &
+./samToIGV.sh BW_16C_WT.R1 &
+./samToIGV.sh BW_16C_WT.R2 &
+./samToIGV.sh BW_2C_atxr.R1 &
+./samToIGV.sh BW_2C_atxr.R2 &
+./samToIGV.sh BW_2C_atxr.R3 &
+./samToIGV.sh BW_2C_atxr.R4 &
+./samToIGV.sh BW_2C_WT.R1 &
+./samToIGV.sh BW_2C_WT.R2 &
+./samToIGV.sh BW_2C_WT.R3 &
+./samToIGV.sh BW_4C_atxr.R1 &
+./samToIGV.sh BW_4C_atxr.R2 &
+./samToIGV.sh BW_4C_atxr.R3 &
+./samToIGV.sh BW_4C_WT.R1 &
+./samToIGV.sh BW_4C_WT.R2 &
+./samToIGV.sh BW_4C_WT.R3 &
+./samToIGV.sh BW_8C_atxr.R1 &
+./samToIGV.sh BW_8C_atxr.R2 &
+./samToIGV.sh BW_8C_atxr.R3 &
+./samToIGV.sh BW_8C_WT.R1 &
+./samToIGV.sh BW_8C_WT.R2 &
+./samToIGV.sh BW_input_atxr &
+./samToIGV.sh BW_input_WT
